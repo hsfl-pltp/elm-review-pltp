5265013693a208e3324b7baa4c9c80d1d285b33b
Mario Köhler <mario.koehler90@gmail.com>
2022-03-29 23:37:22 +0200

    Implement part1 of first version of new-written rule

--- config/ReviewConfig.elm
+++ config/ReviewConfig.elm
@@ -14,6 +14,7 @@ import NoExposingEverything
 import NoForbiddenFeatures
 import NoImportingEverything
 import NoMinimalRecordAccess
+import NoMinimalRecordAccessOld
 import NoMinimalUnderscorePattern
 import NoMissingTypeAnnotation
 import NoNegationOfBooleanOperator
@@ -65,6 +66,7 @@ config =
         }
     , NoImportingEverything.rule []
     , NoMinimalRecordAccess.rule
+    , NoMinimalRecordAccessOld.rule
         { threshold = 3
         , ignoreFunctions = [ "subscriptions" ]
         }
